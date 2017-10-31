9e05f2e5a6c8fe46dfa84533c9b0d6ad1ef53708
Ryan Sperzel <ryansperzel@gmail.com>
2017-10-31 12:59:45 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class EyesOnMe extends React.Component {
+
+  handleFocus(e) {
+    console.log('Good!')
+  }
+
+  handleBlur(e) {
+    console.log('Hey! Eyes on me!')
+  }
+  render() {
+    return (
+      <button onBlur={this.handleBlur} onFocus={this.handleFocus} type="button">Click Me!</button>
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Keypad extends React.Component {
+  handleKeyUp(e) {
+    console.log('Entering password...')
+  }
+  render() {
+    return (
+      <input onKeyUp={this.handleKeyUp} type="password" />
+    )
+  }
+}
+
+export default Keypad
